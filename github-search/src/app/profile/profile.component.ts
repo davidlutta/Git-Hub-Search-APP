c01eaefd2187200211d74a8948e8c0e9dc45ce25
davidlutta <davidlutta2010@gmail.com>
2018-04-28 11:59:28 +0300

    Add css to page

--- github-search/src/app/profile/profile.component.ts
+++ github-search/src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataServiceService } from '../services/data-service.service'
+import { DataServiceService } from '../services/data-service.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,15 +7,32 @@ import { DataServiceService } from '../services/data-service.service'
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  private profile: any[];
+  private profile: string;
+  private username: string;
+  private profile2: string;
+  private repos: string;
 
 
-
-  constructor(private profileInfo:DataServiceService) {
-    this.profileService.getProfileInfo().subscribe(profile =>{
-      console.log(profile);
+  constructor(private profileInfo: DataServiceService) {
+    this.profileInfo.getProfileInfo().subscribe(profile => {
+      // console.log(profile);
       this.profile = profile;
+    });
+
   }
+
+  findProfile() {
+    this.profileInfo.updateProfile(this.username).subscribe(profile2 => {
+      console.log(profile2);
+      this.profile2 = profile2;
+    })
+      this.profileInfo.getprofileRepos(this.username).subscribe(repos => {
+        // console.log(repos)
+        this.repos = repos;
+    })
+
+  }
+
 
   ngOnInit() {
   }
--- github-search/src/app/services/data-service.service.ts
+++ github-search/src/app/services/data-service.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpModule } from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataServiceService {
+  private username: string;
 
-  constructor(private http:Http) {
+  constructor(private http: Http) {
     this.username = "davidlutta";
-   }
+  }
 
-   getProfileInfo(){
-     return this.http.get(environment.link + this.userName + environment.API)
-     .map(res => res.json());
-   }
+  getProfileInfo() {
+    console.log("service Works")
+    return this.http.get(environment.link + this.username + environment.API)
+      .map(res => res.json());
+  }
+  getprofileRepos(username: string) {
+    this.username = username
+    return this.http.get(environment.link + this.username + "/repos" + environment.API).map(res => res.json());
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+    return this.http.get(environment.link + username + environment.API)
+      .map(res => res.json());
+  }
 
 }
