f14d52e9a876d6286fc8944bf9a382554f529261
David Ezekiel Lutta <davidlutta2010@gmail.com>
2020-02-08 16:20:52 +0300

    delete

--- github-search/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  //Link for git hub
-  link: 'https://api.github.com/users/',
-  //client_id
-  clientId:'?client_id=098140ea6fc4e0b2356c',
-  //client_secret
-  clientSecret: '&client_secret=527b28692f1952c2aa097e8f1e2d9f1c3c537b86'
-};
